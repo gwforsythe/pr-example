962b76457943adeb3b66e86667bdaf9f4dda097c
Grant Forsythe <95492751+gwforsythe@users.noreply.github.com>
2022-12-12 11:06:49 -0500

    Add error handling

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ def get_name(n: int) -> str:
     """ Get a random name """
     for _ in range(n):
         response = requests.get('https://api.namefake.com/')
-        yield response.json()['name']
+        if response.ok:
+            yield response.json()['name']
+        else:
+            response.raise_for_status()
 
 
 if __name__ == '__main__':
